d595ac0f7cb7017f12852ac1e90017cf78eb1ca1
barox <joaogomesleite800@gmail.com>
2025-02-15 10:34:43 -0300

    Start

---
+++ src/main/java/com/gomes800/diario_gastronomico/DiarioGastronomicoApplication.java
@@ -0,0 +1,13 @@
+package com.gomes800.diario_gastronomico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarioGastronomicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarioGastronomicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gomes800/diario_gastronomico/DiarioGastronomicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomes800.diario_gastronomico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarioGastronomicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
