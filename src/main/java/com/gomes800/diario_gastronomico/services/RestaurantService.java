fa95c2173d7fde5decf29d63293d93503e52f2e9
Gomes <joaogomesleite800@gmail.com>
2025-02-19 15:32:33 -0300

    feat: create class RestaurantService

---
+++ src/main/java/com/gomes800/diario_gastronomico/services/RestaurantService.java
@@ -0,0 +1,57 @@
+package com.gomes800.diario_gastronomico.services;
+
+import com.gomes800.diario_gastronomico.domain.Restaurant;
+import com.gomes800.diario_gastronomico.repositories.RestaurantRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class RestaurantService {
+
+    private RestaurantRepository repository;
+
+    public List<Restaurant> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Restaurant> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Restaurant insert(Restaurant obj) {
+        return repository.save(obj);
+    }
+
+    public void updateData(Restaurant entity, Restaurant obj) {
+        entity.setName(obj.getName());
+        entity.setAddress(obj.getAddress());
+        entity.setRating(obj.getRating());
+        entity.setComment(obj.getComment());
+    }
+
+    public Restaurant update(Long id, Restaurant obj) {
+        try {
+            Restaurant entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            if (repository.existsById(id)) {
+                repository.deleteById(id);
+            } else {
+                throw new EntityNotFoundException("Restaurant not found with ID: " + id);
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Database integrity violation: " + e.getMessage());
+        }
+    }
+}
