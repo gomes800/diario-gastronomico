eebfb6838c53d13a275090bb705a323ca3a92e1d
Gomes <joaogomesleite800@gmail.com>
2025-02-19 15:11:16 -0300

    feat: create class UserService

---
+++ src/main/java/com/gomes800/diario_gastronomico/services/UserService.java
@@ -0,0 +1,58 @@
+package com.gomes800.diario_gastronomico.services;
+
+import com.gomes800.diario_gastronomico.domain.User;
+import com.gomes800.diario_gastronomico.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public Optional<User> findByID(Long id) {
+        return repository.findById(id);
+    }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+    }
+
+    public User update(Long id, User obj) {
+        try {
+            User entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void delete(Long id) {
+        try {
+            if (repository.existsById(id)) {
+                repository.deleteById(id);
+            } else {
+                throw new EntityNotFoundException("User not found with ID: " + id);
+            }
+        } catch (DataIntegrityViolationException e) {
+            throw new RuntimeException("Database integrity violation: " + e.getMessage());
+        }
+    }
+
+}
