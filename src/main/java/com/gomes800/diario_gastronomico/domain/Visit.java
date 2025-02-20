0c034cfdcf954e448ae4e4d08cbfb2380c78edd2
Gomes <joaogomesleite800@gmail.com>
2025-02-20 10:14:38 -0300

    feat: create class Visit

---
+++ src/main/java/com/gomes800/diario_gastronomico/domain/Visit.java
@@ -0,0 +1,38 @@
+package com.gomes800.diario_gastronomico.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_visita")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Visit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    private byte rating;
+    private String comment;
+    private LocalDateTime visitDate;
+
+    @OneToMany(mappedBy = "visita", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Photo> photos;
+}
