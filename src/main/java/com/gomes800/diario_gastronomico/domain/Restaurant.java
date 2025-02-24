89f48ec4e9ad78021caa34d1cfa15895bf6cca05
Gomes <joaogomesleite800@gmail.com>
2025-02-24 09:46:07 -0300

    feat: implements serializable

--- src/main/java/com/gomes800/diario_gastronomico/domain/Restaurant.java
+++ src/main/java/com/gomes800/diario_gastronomico/domain/Restaurant.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -15,7 +16,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class Restaurant {
+public class Restaurant implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
