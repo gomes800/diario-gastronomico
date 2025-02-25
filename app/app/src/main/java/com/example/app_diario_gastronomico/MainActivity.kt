0d44504cd0c3afa8a4b056fa074b3376412aca92
gomes <joaogomesleite800@gmail.com>
2025-02-25 15:06:02 -0300

    feat: Example screens

--- app/app/src/main/java/com/example/app_diario_gastronomico/MainActivity.kt
+++ app/app/src/main/java/com/example/app_diario_gastronomico/MainActivity.kt
@@ -3,45 +3,55 @@ package com.example.app_diario_gastronomico
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.app_diario_gastronomico.ui.theme.App_diario_gastronomicoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            App_diario_gastronomicoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            TelaInicial()
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TelaInicial() {
+    var contador by remember { mutableIntStateOf(0) }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    App_diario_gastronomicoTheme {
-        Greeting("Android")
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF4C92CB)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Bem-vindo ao Diário Gastronômico!", fontSize = 20.sp, color = Color.DarkGray)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = { contador++ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF6200EE)),
+                shape = RoundedCornerShape(10.dp),
+                modifier = Modifier.padding(8.dp)
+            ) {
+                Text(text = "Entrar", color = Color.White)
+            }
+        }
     }
 }
---
+++ app/app/src/main/java/com/example/app_diario_gastronomico/ui/HomeScreen.kt
@@ -0,0 +1,47 @@
+package com.example.app_diario_gastronomico.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Preview
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF3F3EC)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(text = "Início", fontSize = 24.sp)
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = {}) {
+                Text("Restaurantes")
+            }
+        }
+    }
+
+}
