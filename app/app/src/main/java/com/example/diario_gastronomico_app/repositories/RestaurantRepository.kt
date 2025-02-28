4519490bd77e63a2391e8b53b1ef50f496a7bf32
gomes <joaogomesleite800@gmail.com>
2025-02-28 12:59:42 -0300

    feat: create RestaurantRepository

---
+++ app/app/src/main/java/com/example/diario_gastronomico_app/repositories/RestaurantRepository.kt
@@ -0,0 +1,29 @@
+package com.example.diario_gastronomico_app.repositories
+
+import com.example.diario_gastronomico_app.domain.Restaurant
+import com.example.diario_gastronomico_app.network.RetrofitClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class RestaurantRepository {
+
+    suspend fun getRestaurants(): List<Restaurant> {
+        return withContext(Dispatchers.IO) {
+            try {
+                RetrofitClient.apiService.getRestaurants()
+            } catch (e: Exception) {
+                emptyList()
+            }
+        }
+    }
+
+    suspend fun getRestaurantById(id: Long): Restaurant? {
+        return withContext(Dispatchers.IO) {
+            try {
+                RetrofitClient.apiService.getRestaurantById(id)
+            } catch (e: Exception) {
+                null
+            }
+        }
+    }
+}
