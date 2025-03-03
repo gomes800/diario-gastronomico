4f2905eaf54688c5eb99ba63296d6d20c6a65d67
gomes <joaogomesleite800@gmail.com>
2025-03-03 09:54:26 -0300

    feat: update RetrofitClient

--- app/app/src/main/java/com/example/diario_gastronomico_app/network/RetrofitClient.kt
+++ app/app/src/main/java/com/example/diario_gastronomico_app/network/RetrofitClient.kt
@@ -1,16 +1,29 @@
 package com.example.diario_gastronomico_app.network
 
+import com.example.diario_gastronomico_app.domain.Visit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "http://192.168.1.158:8080/"
 
-    val restaurantService: RestaurantService by lazy {
-        Retrofit.Builder()
+    private fun <T> createService(service: Class<T>): T {
+        return Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(RestaurantService::class.java)
+            .create(service)
+    }
+
+    val restaurantService: RestaurantService by lazy {
+        createService(RestaurantService::class.java)
+    }
+
+    val visitService: VisitService by lazy {
+        createService(VisitService::class.java)
+    }
+
+    val userService: UserService by lazy {
+        createService(UserService::class.java)
     }
 }
