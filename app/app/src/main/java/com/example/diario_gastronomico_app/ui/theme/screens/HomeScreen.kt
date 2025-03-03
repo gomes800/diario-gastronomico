8ddd8de65efbd5bc176f71a7495a19d8876b606c
gomes <joaogomesleite800@gmail.com>
2025-03-03 09:11:59 -0300

    style: visual adjustments

--- app/app/src/main/java/com/example/diario_gastronomico_app/ui/theme/screens/HomeScreen.kt
+++ app/app/src/main/java/com/example/diario_gastronomico_app/ui/theme/screens/HomeScreen.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,21 +36,38 @@ fun HomeScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color(0xFF4C92CB))
+            .background(Color(0xFF8EA8C3))
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
+            verticalArrangement = Arrangement.Center
         ) {
             Spacer(modifier = Modifier.height(32.dp))
 
-            Text(text = "Início", fontSize = 24.sp, color = Color.White)
+            Text(text = "Tela inicial",
+                fontSize = 24.sp,
+                color = Color.Black,
+                modifier = Modifier.padding(bottom = 32.dp))
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(100.dp))
 
-            Button(onClick = { navController.navigate("restaurants") }) {
-                Text("Restaurantes")
+            Button(
+                onClick = { navController.navigate("restaurants") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF23395B))) {
+                Text("Meus Restaurantes",
+                    style = TextStyle(fontSize = 20.sp),
+                    color = Color.White
+                )
+            }
+            Button(
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF23395B))) {
+                Text("Favoritos",
+                    style = TextStyle(fontSize = 20.sp),
+                    color = Color.White)
             }
         }
     }
