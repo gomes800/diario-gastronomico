54c82dc09a55ba3b068ee08b68f61e86ecd27cbf
gomes <joaogomesleite800@gmail.com>
2025-03-03 09:54:04 -0300

    feat: create UserService, VisitService, VisitRepository

---
+++ app/app/src/main/java/com/example/diario_gastronomico_app/network/VisitService.kt
@@ -0,0 +1,4 @@
+package com.example.diario_gastronomico_app.network
+
+interface VisitService {
+}
---
+++ app/app/src/main/java/com/example/diario_gastronomico_app/repositories/VisitRepository.kt
@@ -0,0 +1,18 @@
+package com.example.diario_gastronomico_app.repositories
+
+import com.example.diario_gastronomico_app.domain.Visit
+import com.example.diario_gastronomico_app.network.RetrofitClient
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class VisitRepository {
+
+    suspend fun getVisits(): List<Visit> {
+        return withContext(Dispatchers.IO) {
+            try {
+                RetrofitClient.
+            }
+        }
+    }
+
+}
