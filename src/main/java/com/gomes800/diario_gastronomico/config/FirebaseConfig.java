eed83b5cc4e5c2b08f2ec42f2770b8b04bba8955
Gomes <joaogomesleite800@gmail.com>
2025-03-06 10:48:01 -0300

    feat: add Firebase storage

---
+++ src/main/java/com/gomes800/diario_gastronomico/config/FirebaseConfig.java
@@ -0,0 +1,26 @@
+package com.gomes800.diario_gastronomico.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public void initializeFirebase() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/diario-gastronomico-firebase-adminsdk-fbsvc-3f3e079a4c.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("diario-gastronomico.appspot.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+    }
+}
