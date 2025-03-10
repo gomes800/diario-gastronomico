0eb979a63a22246e0b6bb248a2d20120d657bbac
Gomes <joaogomesleite800@gmail.com>
2025-03-10 10:15:37 -0300

    feat: add placeId in class Restaurant

--- src/main/java/com/gomes800/diario_gastronomico/domain/Restaurant.java
+++ src/main/java/com/gomes800/diario_gastronomico/domain/Restaurant.java
@@ -21,6 +21,9 @@ public class Restaurant implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
+    private String placeId;
     private String name;
     private String address;
     private String phone;
