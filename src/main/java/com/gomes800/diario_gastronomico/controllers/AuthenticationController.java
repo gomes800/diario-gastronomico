5fd3c22cc2492b09dd95e4e726dd7a489772f097
gomes <joaogomesleite800@gmail.com>
2025-03-22 12:06:32 -0300

    feat: add new method register

--- src/main/java/com/gomes800/diario_gastronomico/controllers/AuthenticationController.java
+++ src/main/java/com/gomes800/diario_gastronomico/controllers/AuthenticationController.java
@@ -1,11 +1,15 @@
 package com.gomes800.diario_gastronomico.controllers;
 
+import com.gomes800.diario_gastronomico.domain.User;
 import com.gomes800.diario_gastronomico.domain.dto.AuthenticationDTO;
+import com.gomes800.diario_gastronomico.domain.dto.RegisterDTO;
+import com.gomes800.diario_gastronomico.repositories.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +23,25 @@ public class AuthenticationController {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserRepository repository;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
         var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
         var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data) {
+        if (this.repository.findByLogin(data.login()) != null) return ResponseEntity.badRequest().build();
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.password());
+        User newUser = new User(data.login(), encryptedPassword, data.role());
+
+        repository.save(newUser);
 
         return ResponseEntity.ok().build();
     }
--- src/main/java/com/gomes800/diario_gastronomico/domain/dto/RegisterDTO.java
+++ src/main/java/com/gomes800/diario_gastronomico/domain/dto/RegisterDTO.java
@@ -2,5 +2,5 @@ package com.gomes800.diario_gastronomico.domain.dto;
 
 import com.gomes800.diario_gastronomico.domain.UserRole;
 
-public record RegisterDTO(String login, String password, String role) {
+public record RegisterDTO(String login, String password, UserRole role) {
 }
