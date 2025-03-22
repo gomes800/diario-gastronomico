79ef9a1ff0e336a803ce9e0551a8427b54c6ca93
gomes <joaogomesleite800@gmail.com>
2025-03-22 09:31:34 -0300

    feat: add enum UserRole

---
+++ src/main/java/com/gomes800/diario_gastronomico/domain/UserRole.java
@@ -0,0 +1,16 @@
+package com.gomes800.diario_gastronomico.domain;
+
+public enum UserRole {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+    UserRole(String role){
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
