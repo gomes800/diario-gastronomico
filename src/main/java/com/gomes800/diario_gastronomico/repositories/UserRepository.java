de27083b1bf351d9e54fe2481604679ae04fdb57
gomes <joaogomesleite800@gmail.com>
2025-03-22 09:42:52 -0300

    feat: add method findByLogin

--- src/main/java/com/gomes800/diario_gastronomico/repositories/UserRepository.java
+++ src/main/java/com/gomes800/diario_gastronomico/repositories/UserRepository.java
@@ -2,6 +2,9 @@ package com.gomes800.diario_gastronomico.repositories;
 
 import com.gomes800.diario_gastronomico.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByLogin(String login);
 }
