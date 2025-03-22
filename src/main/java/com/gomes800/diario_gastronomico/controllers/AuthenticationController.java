9ab9560c9c769c5b08f09d370fdbc66026e46896
gomes <joaogomesleite800@gmail.com>
2025-03-22 11:33:08 -0300

    feat: create AuthenticationController

---
+++ src/main/java/com/gomes800/diario_gastronomico/controllers/AuthenticationController.java
@@ -0,0 +1,30 @@
+package com.gomes800.diario_gastronomico.controllers;
+
+import com.gomes800.diario_gastronomico.domain.dto.AuthenticationDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("auth")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+}
