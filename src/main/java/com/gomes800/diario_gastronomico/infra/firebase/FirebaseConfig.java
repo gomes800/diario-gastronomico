3df374884ba669e6c178c6302b202443248fb7a6
gomes <joaogomesleite800@gmail.com>
2025-03-22 11:27:20 -0300

    feat: add jakarta validation

--- src/main/java/com/gomes800/diario_gastronomico/infra/firebase/FirebaseConfig.java
+++ src/main/java/com/gomes800/diario_gastronomico/infra/firebase/FirebaseConfig.java
@@ -1,4 +1,4 @@
-package com.gomes800.diario_gastronomico.config;
+package com.gomes800.diario_gastronomico.infra.firebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
